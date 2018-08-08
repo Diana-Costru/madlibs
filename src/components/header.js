f703ea1c1ef0b0224a8050e90c94a05891571936
Diana-Costru <diana.costru@gmail.com>
2018-08-08 20:03:07 +0300

    source of project

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+        <div className="header__skew"></div>
+        <div className="header_border"></div>
+        <h1>Bottega Madlibs</h1>
+        <p>
+        Fill out the fields bellow and click the generate button to see the Mad Lib story.
+        </p>
+        </div>
+    )
+}
+
+export default Header;
